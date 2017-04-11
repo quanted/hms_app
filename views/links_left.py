85640c0ece8209b666cceeb10f3cf48dd1c638ea
Tom Purucker <purucker.tom@gmail.com>
2017-04-11 20:34:26 +0000

    lowercase hydrology refactor for urls.py

--- views/links_left.py
+++ views/links_left.py
@@ -5,7 +5,7 @@ from collections import OrderedDict
 def ordered_list(model, submodel, page=None):
     link_dict = OrderedDict([
         ('Components', OrderedDict([
-            ('Hydrology', 'hydrology'),
+            ('hydrology', 'hydrology'),
             ('Water Quality', 'water_quality'),])),
         ('Utilities', OrderedDict([
             ('API Documentation', 'api_doc'),
