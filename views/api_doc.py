938d58ab9039fe515fdf16ceeed973bc7f56ad72
dbsmith88 <dbsmith@uga.edu>
2017-10-13 08:55:09 -0500

    updated swagger json endpoint to new swagger.json url for .Net core backend

--- views/api_doc.py
+++ views/api_doc.py
@@ -20,8 +20,8 @@ def get_swagger_json(request):
     """
     Opens up swagger.json content
     """
-    url = str(os.environ.get('HMS_BACKEND_SERVER')) + '/HMSWS/swagger/docs/v1'  # HMS backend server for swagger
-    # url = str(os.environ.get('HMS_BACKEND_SERVER')) + '/HMSWS/swagger/v1/swagger.json'  # .NET core backend
+    #url = str(os.environ.get('HMS_BACKEND_SERVER')) + '/HMSWS/swagger/docs/v1'  # HMS backend server for swagger
+    url = str(os.environ.get('HMS_BACKEND_SERVER')) + '/HMSWS/swagger/v1/swagger.json'  # .NET core backend
     swagger = requests.get(url)
     swagger = json.loads(swagger.content)
     swagger["host"] = str(os.environ.get('HMS_BACKEND_SERVER'))  # changes internal ip to external ip
