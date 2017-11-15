d9ca567f3abd552a746ba949ac2a3d6d78b91705
dbsmith88 <dbsmith@uga.edu>
2017-11-15 12:50:08 -0600

    corrected request parameter error

--- views/output.py
+++ views/output.py
@@ -38,29 +38,29 @@ def hydrology_output_page(request, model='hydrology', submodel='', header=''):
     if form.is_valid():
         parameters = form.cleaned_data
 
-        # request_parameters = {
-        #     "source": str(parameters['source']).lower(),
-        #     "dateTimeSpan": {
-        #         "startDate": str(parameters['startDate']),
-        #         "endDate": str(parameters['endDate'])
-        #     },
-        #     "geometry": {
-        #         "point": {
-        #             "latitude": str(parameters['latitude']),
-        #             "longitude": str(parameters['longitude'])
-        #         },
-        #         "geometryMetadata": set_geometry_metadata(parameters["spatial_metadata"])
-        #     },
-        #     "temporalResolution": str(parameters['temporalresolution']),
-        #     "timeLocalized": str(parameters['localTime'])
-        # }
-        # if "soilmoisture" in submodel:
-        #     request_parameters["layers"] = parameters["layers"]
+        request_parameters = {
+            "source": str(parameters['source']).lower(),
+            "dateTimeSpan": {
+                "startDate": str(parameters['startDate']),
+                "endDate": str(parameters['endDate'])
+            },
+            "geometry": {
+                "point": {
+                    "latitude": str(parameters['latitude']),
+                    "longitude": str(parameters['longitude'])
+                },
+                "geometryMetadata": set_geometry_metadata(parameters["geometrymetadata"])
+            },
+            "temporalResolution": str(parameters['temporalresolution']),
+            "timeLocalized": str(parameters['timelocalized'])
+        }
+        if "soilmoisture" in submodel:
+            request_parameters["layers"] = parameters["layers"]
 
-        target = str(os.environ.get('HMS_BACKEND_SERVER')) + '/HMSWS/api/' + submodel
-        swaggernurl = "https://qedinternal.epa.gov/hms/api_doc/swagger"
-        request_parameters = swag(method="POST", target_url=target, parameters=parameters, swagger_url=swaggernurl)
-        data = get_data(submodel, request_parameters.request_object)
+        # target = str(os.environ.get('HMS_BACKEND_SERVER')) + '/HMSWS/api/' + submodel
+        # swaggernurl = "https://qedinternal.epa.gov/hms/api_doc/swagger"
+        # request_parameters = swag(method="POST", target_url=target, parameters=parameters, swagger_url=swaggernurl)
+        data = get_data(submodel, request_parameters)
         location = str(parameters['latitude']) + ", " + str(parameters['longitude'])
         html = create_output_page(model, submodel, data, submodel.capitalize(), location)
     else:
@@ -96,28 +96,28 @@ def precip_compare_output_page(request, model='precip_compare', header=''):
     form = input_form(request.POST)
     if form.is_valid():
         parameters = form.cleaned_data
-        # request_parameters = {
-        #     "dataset": "Precipitation",
-        #     "source": "compare",
-        #     "dateTimeSpan": {
-        #         "startDate": str(parameters['startDate']),
-        #         "endDate": str(parameters['endDate'])
-        #     },
-        #     "geometry": {
-        #         "geometryMetadata": {
-        #             "stationID": str(parameters['stationID'])
-        #         }
-        #     },
-        #     "timeLocalized": "true"
-        # }
-        parameters["source"] = "compare"
-        stationID = {}
-        stationID["stationID"] = parameters["stationID"]
-        parameters["geometryMetadata"] = stationID
-        target = str(os.environ.get('HMS_BACKEND_SERVER')) + '/HMSWS/api/precipitation'
-        jsonurl = "https://qedinternal.epa.gov/hms/api_doc/swagger"
-        request_parameters = swag(method="POST", target_url=target, parameters=parameters, swagger_url=jsonurl).request_object
-        request_parameters["dataset"] = "Precipitation"
+        request_parameters = {
+            "dataset": "Precipitation",
+            "source": "compare",
+            "dateTimeSpan": {
+                "startDate": str(parameters['startDate']),
+                "endDate": str(parameters['endDate'])
+            },
+            "geometry": {
+                "geometryMetadata": {
+                    "stationID": str(parameters['stationID'])
+                }
+            },
+            "timeLocalized": "true"
+        }
+        # parameters["source"] = "compare"
+        # stationID = {}
+        # stationID["stationID"] = parameters["stationID"]
+        # parameters["geometryMetadata"] = stationID
+        # target = str(os.environ.get('HMS_BACKEND_SERVER')) + '/HMSWS/api/precipitation'
+        # jsonurl = "https://qedinternal.epa.gov/hms/api_doc/swagger"
+        # request_parameters = swag(method="POST", target_url=target, parameters=parameters, swagger_url=jsonurl).request_object
+        # request_parameters["dataset"] = "Precipitation"
         data = get_compare_data("precip_compare", request_parameters)
         location = str(parameters['stationID'])
         html = create_output_page(model, model, data, "Precipitation", location)
@@ -163,7 +163,7 @@ def get_data(submodel, parameters):
     """
     # url = 'http://134.67.114.8/HMSWS/api/' + submodel                                  # server 8 HMS, external
     # url = 'http://172.20.10.18/HMSWS/api/WSHMS/'                                  # server 8 HMS, internal
-    # url = 'http://localhost:52569/api/' + submodel                                  # local VS HMS
+    # url = 'http://localhost:60049/api/' + submodel                                  # local VS HMS
     # url = 'http://localhost:7777/rest/hms/'                                       # local flask
     url = str(os.environ.get('HMS_BACKEND_SERVER')) + '/HMSWS/api/' + submodel    # HMS backend server variable
     print("url: " + url)
@@ -188,17 +188,17 @@ def get_compare_data(model, parameters):
     """
     url = ""
     if model == "precip_compare":
-        url = 'http://134.67.114.8/HMSWS/api/Precipitation/'                              # server 8 HMS, external
+        # url = 'http://134.67.114.8/HMSWS/api/Precipitation/'                              # server 8 HMS, external
         # url = 'http://172.20.10.18/HMSWS/api/WSPrecipitation/'                            # server 8 HMS, internal
-        # url = 'http://localhost:52569/api/workflow/compare'                              # local VS HMS
+        # url = 'http://localhost:60049/api/Precipitation/'                              # local VS HMS
         # url = 'http://localhost:7777/hms/rest/Precipitation/'                             # local flask
-        #url = str(os.environ.get('HMS_BACKEND_SERVER')) + '/HMSWS/api/workflow/compare'   # HMS backend server variable
+        url = str(os.environ.get('HMS_BACKEND_SERVER')) + '/HMSWS/api/Precipitation'   # HMS backend server variable
     elif model == "runoff_compare":
         # url = 'http://134.67.114.8/HMSWS/api/WSLandSurfaceFlow/'                             # server 8 HMS, external
         # url = 'http://172.20.10.18/HMSWS/api/WSLandSurfaceFlow/'                             # server 8 HMS, internal
-        # url = 'http://localhost:50052/api/WSLandSurfaceFlow/'                                       # local VS HMS
+        # url = 'http://localhost:60049/api/LandSurfaceFlow/'                                       # local VS HMS
         # url = 'http://localhost:7777/hms/rest/LandSurfaceFlow/'                                   # local flask
-        url = str(os.environ.get('HMS_BACKEND_SERVER')) + '/HMSWS/api/WSLandSurfaceFlow/'  # HMS backend server variable
+        url = str(os.environ.get('HMS_BACKEND_SERVER')) + '/HMSWS/api/LandSurfaceFlow/'  # HMS backend server variable
     try:
         result = requests.post(str(url), json=parameters, timeout=10000)
     except requests.exceptions.RequestException as e:
@@ -239,13 +239,13 @@ def create_output_page(model, submodel, data, dataset, location):
     try:
         columns = {}
         ncolumns = 0
-        if "ERROR" not in data["Metadata"]:
-            ncolumns = len(data["Data"][list(data["Data"].keys())[0]])
-        for key in data["Metadata"]:
+        if "ERROR" not in data["metadata"]:
+            ncolumns = len(data["data"][list(data["data"].keys())[0]])
+        for key in data["metadata"]:
             if "column_" in key:
-                columns[key] = data["Metadata"][key]
+                columns[key] = data["metadata"][key]
         if len(columns) < ncolumns:
-            for key in data["Metadata"]:
+            for key in data["metadata"]:
                 if "timeseries_" in key:
                     k = key.split('_')
                     columns[str(k[len(k)-1])] = data["metadata"][key]
@@ -259,18 +259,18 @@ def create_output_page(model, submodel, data, dataset, location):
         dcolumns = columns[1:len(columns)]
         for datasets in dcolumns:
             dstats = [datasets,
-                      data["Metadata"].get(datasets + "_average", ""),
-                      data["Metadata"].get(datasets + "_sum", ""),
-                      data["Metadata"].get(datasets + "_standard_deviation", ""),
-                      data["Metadata"].get(datasets + "_ncdc_R-Squared", ""),
-                      data["Metadata"].get(datasets + "_ncdc_gore", "")]
+                      data["metadata"].get(datasets + "_average", ""),
+                      data["metadata"].get(datasets + "_sum", ""),
+                      data["metadata"].get(datasets + "_standard_deviation", ""),
+                      data["metadata"].get(datasets + "_ncdc_R-Squared", ""),
+                      data["metadata"].get(datasets + "_ncdc_gore", "")]
             stats.append(dstats)
         html += render_to_string('04hms_output_table_2.html', {
             'MODEL': model,
             'SUBMODEL': submodel,
             'TITLE': "HMS " + model.replace('_', ' ').title() + " Data",
-            'METADATA': data["Metadata"],
-            'DATA': data["Data"],
+            'METADATA': data["metadata"],
+            'DATA': data["data"],
             'COLUMNS': columns,
             'STATS': stats,
             'DATASET': dataset,
