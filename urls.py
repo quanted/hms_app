e44ec8c4aad66d07194d4444401e920c93ee4983
dbsmith88 <dbsmith@uga.edu>
2018-01-09 11:26:43 -0600

    django 2.0 urls for hms_app

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 #  https://docs.djangoproject.com/en/1.6/intro/tutorial03/
 from django.conf import settings
 from django.conf.urls import include, url
+from django.urls import path
 from .views import description, landing, hydrology_submodels, output, watershed_map
 from .views import runoff_compare_setup, precip_compare_setup, water_quality_submodels, api_doc
 from .models.water_quality import output as wq_output
@@ -15,31 +16,38 @@ if settings.IS_PUBLIC:
     ]
 else:
     urlpatterns = [
-        # url(r'^api/', include('api.urls')),
-        # url(r'^rest/', include('REST.urls')),
-        url(r'^$', landing.hms_landing_page),
-        url(r'^precip_compare/$', precip_compare_setup.input_page),
-        url(r'^precip_compare/output/?$', output.precip_compare_output_page),
-        url(r'^runoff_compare/$', runoff_compare_setup.input_page),
-        url(r'^runoff_compare/output/$', output.runoff_compare_output_page),
-        url(r'^hydrology/(?P<submodel>\w+)/$', hydrology_submodels.submodel_page),
-        url(r'^hydrology/(?P<submodel>\w+)/output/?$', output.hydrology_output_page),
-        url(r'^water_quality/(?P<submodel>\w+)/$', water_quality_submodels.submodel_page),
-        url(r'^water_quality/(?P<submodel>\w+)/output$', wq_output.water_quality_output),
-        url(r'^water_quality/(?P<submodel>\w+)/output/json$', wq_output.water_quality_json_output),
-        url(r'^watershed$', watershed_map.hms_map_page),
-        url(r'^api_doc/$', api_doc.create_swagger_docs),
-        url(r'^api_doc/swagger$', api_doc.get_swagger_json),
-        url(r'^(?P<model>\w+)/$', description.description_page),
 
-        # url(r'^$', views.qed_splash_page_intranet),
-        # url(r'^admin/', include(admin.site.urls)),
+        # django 1.11
+        # url(r'^$', landing.hms_landing_page),
+        # url(r'^precip_compare/$', precip_compare_setup.input_page),
+        # url(r'^precip_compare/output/?$', output.precip_compare_output_page),
+        # url(r'^runoff_compare/$', runoff_compare_setup.input_page),
+        # url(r'^runoff_compare/output/$', output.runoff_compare_output_page),
+        # url(r'^hydrology/(?P<submodel>\w+)/$', hydrology_submodels.submodel_page),
+        # url(r'^hydrology/(?P<submodel>\w+)/output/?$', output.hydrology_output_page),
+        # url(r'^water_quality/(?P<submodel>\w+)/$', water_quality_submodels.submodel_page),
+        # url(r'^water_quality/(?P<submodel>\w+)/output$', wq_output.water_quality_output),
+        # url(r'^water_quality/(?P<submodel>\w+)/output/json$', wq_output.water_quality_json_output),
+        # url(r'^watershed$', watershed_map.hms_map_page),
+        # url(r'^api_doc/$', api_doc.create_swagger_docs),
+        # url(r'^api_doc/swagger$', api_doc.get_swagger_json),
+        # url(r'^(?P<model>\w+)/$', description.description_page),
 
-        # rest urls
-        # url(r'^rest$', hms_rest_api.pass_to_hms, name='hms_post_rest'),
-        # url(r'^rest/(?P<submodel>w+)$', hms_rest_api.pass_post_to_hms, name='hms_submodel_post_rest'),
-        # url(r'rest/(?P<submodel>w+)/(<parameters>)', hms_rest_api.pass_get_to_hms, name='hms_submodel_get_rest'),
-        # url(r'^rest/precip_compare$', hms_rest_api.pass_precip_compare_to_hms, name='hms_precip_compare_rest')
+        # django 2.0
+        path('', landing.hms_landing_page),
+        path('precip_compare', precip_compare_setup.input_page),
+        path('precip_compare/output', output.precip_compare_output_page),
+        path('runoff_compare', runoff_compare_setup.input_page),
+        path('runoff_compare/output', output.runoff_compare_output_page),
+        path('hydrology/<slug:submodel>', hydrology_submodels.submodel_page),
+        path('hydrology/<slug:submodel>/output', output.hydrology_output_page),
+        path('water_quality/<slug:submodel>', water_quality_submodels.submodel_page),
+        path('water_quality/<slug:submodel>/output', wq_output.water_quality_output),
+        path('water_quality/<slug:submodel>/output/json', wq_output.water_quality_json_output),
+        path('watershed', watershed_map.hms_map_page),
+        path('api_doc', api_doc.create_swagger_docs),
+        path('api_doc/swagger', api_doc.get_swagger_json),
+        path('<slug:model>', description.description_page),
     ]
 
 # 404 Error view (file not found)
