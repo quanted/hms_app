618c8c28c684cffda9c8a7c14f0fc6ef05b36c4b
dbsmith88 <dbsmith@uga.edu>
2019-03-08 15:29:41 -0600

    updated swagger config settings

--- views/api_doc.py
+++ views/api_doc.py
@@ -33,18 +33,22 @@ def get_swagger_json(request):
     swagger = json.loads(swagger.content)
     if os.environ['HMS_LOCAL'] == "True":
         swagger["host"] = "127.0.0.1:8000/hms/rest"
-    elif os.environ['HMS_BACKEND_SERVER_DOCKER'] == "http://172.20.100.11:7778":
-        swagger["host"] = "qedinternal.epa.gov/hms/rest"
+        print("HMS Local")
+    elif os.environ['HMS_BACKEND_SERVER_DOCKER'] == "http://172.16.0.4:7778":
+        swagger["host"] = "172.16.0.4/hms/rest"
         swagger["basePath"] = ""
+        print("HMS AZURE Backend Docker")
     elif os.environ['HMS_BACKEND_SERVER_DOCKER'] == "http://172.20.100.15:7778":
         swagger["host"] = "134.67.114.5/hms/rest"
         swagger["basePath"] = ""
+        print("HMS CGI server 5")
     # elif os.environ['IN_DOCKER'] == "True":
     #     swagger["host"] = "172.20.100.11/hms/rest/"
     #     swagger["basePath"] = ""
     else:
         swagger["host"] = "qedinternal.epa.gov/hms/rest"
         swagger["basePath"] = ""
+        print("HMS qedinteral")
     response = HttpResponse()
     response.write(json.dumps(swagger))
     return response
