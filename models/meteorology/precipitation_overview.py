6884ebfa4c2136752fa2c5a334f758f921872ab8
dbsmith88 <dbsmith@uga.edu>
2019-11-06 08:09:06 -0600

    bug fix

--- models/meteorology/precipitation_overview.py
+++ models/meteorology/precipitation_overview.py
@@ -33,7 +33,7 @@ class Precipitation:
                                "0.25-degrees covering the Earth between December 2016.",
         "DAYMET Precipitation": "Daymet is a daily dataset of rain gauge data that has been interpolated and extrapolated. "
                                 "Daymet uses ground station data with their model algorithm to produce gridded estimates "
-                               "of daily weather parameters. The interpolated spatial resolution is about a 0.009-degree "
+                                "of daily weather parameters. The interpolated spatial resolution is about a 0.009-degree "
                                 "grid over North America. Data is accessible since 1980 to the latest full year.",
         "PRISM Precipitation": "The Parameter-elevation Relationship on Independent Slopes Model (PRISM) is a combined "
                                "dataset consisting of ground gauge station and RADAR products. The data is on a 4km grid "
@@ -66,45 +66,45 @@ class Precipitation:
         ["Start Date", "String", "Start date for the output timeseries. e.g., 01/01/2010"],
         ["End Date", "String", "End date for the output timeseries. e.g., 12/31/2010"],
         ["Output Date Format", "String", "Format of the datetime stamp of the output timeseries. Valid options can be found "
-                                        "here: <a href=\"https://docs.microsoft.com/en-us/dotnet/api/system.datetime.tostring?view=netcore-2.2\" target=\"_blank\">Microsoft Documentation</a>"],
+                                         "here: <a href=\"https://docs.microsoft.com/en-us/dotnet/api/system.datetime.tostring?view=netcore-2.2\" target=\"_blank\">Microsoft Documentation</a>"],
         ["Latitude", "Number", "Latitude coordinate for the output timeseries. e.g., 33.925575"],
         ["Longitude", "Number", "Longitude coordinate for the output timeseries. e.g., -83.356893"],
         ["NCEI StationID", "String", "NOAA NCEI station identification number (available if source set to 'ncei'). e.g., GHCND:USW00013874 ."],
 
         ["Temporal Resolution", "String", "Temporal resolution/timestep of the output timeseries. Options are limited by the "
-                                     "default timestep of the data source. All options are: 'default', 'daily', 'weekly', 'monthly'."],
+                                          "default timestep of the data source. All options are: 'default', 'daily', 'weekly', 'monthly'."],
         ["Output Data Format", "String", "Format of the returned API object. Valid options are: 'json'."],
         ["Local Time", "Boolean",
          "Specify if the date/timestamp on the output timeseries is set to the local timezone of the spatial area of interest or to GMT."],
     ]
-        '''--------------------discarded documentation:---------------------
-        ["dateTimeSpan", "Dictionary", "Object holding the timeseries temporal input parameters "
-                                       "(startDate, endDate, dateTimeFormat)"],
-        ["geometry", "Dictionary", "Object holding the timeseries spatial input parameters. (point, stationID)"],
-        ["point", "Dictionary", "Object holding point coordinate parameters. (latitude, longitude)"],
-        ["dataValueFormat", "String", "Format of the output timeseries data values. Valid options can be found here: "
-                                      "<a href=\"https://docs.microsoft.com/en-us/dotnet/api/system.double.tostring?view=netcore-2.2\" target=\"_blank\">Microsoft Documentation</a>"],
-        ["localTime", "Boolean", "Specify if the timestamp on the output timeseries is set to the timezone of the spatial area of interest."],
-        ["units", "String", "Units of the output timeseries. Valid options are: 'default', 'metric', 'imperial'"],
-        '''
-    # Output return object are provided as a list of lists, each list containing 3 elements: column,
-    # datatype and description.
-    output_object = [
-        ["dataset", "String", "Primary dataset of the requested timeseries. Some API calls return more than one dataset, "
-                             "either for a workflow API or other relevent dataset."],
-        ["dataSource", "String", "Primary source of the requested timeseries."],
-        ["metaData", "Dictionary", "Metadata for the output timeseries, includes metadata from the source as well "
-                                   "as HMS metadata."],
-        ["data", "Dictionary", "Output timeseries data is returned as a dictionary, where the key is the datetime stamp "
-                               "and value is a list of values for the source/dataset."]
-    ]
+    '''--------------------discarded documentation:---------------------
+    ["dateTimeSpan", "Dictionary", "Object holding the timeseries temporal input parameters "
+                                   "(startDate, endDate, dateTimeFormat)"],
+    ["geometry", "Dictionary", "Object holding the timeseries spatial input parameters. (point, stationID)"],
+    ["point", "Dictionary", "Object holding point coordinate parameters. (latitude, longitude)"],
+    ["dataValueFormat", "String", "Format of the output timeseries data values. Valid options can be found here: "
+                                  "<a href=\"https://docs.microsoft.com/en-us/dotnet/api/system.double.tostring?view=netcore-2.2\" target=\"_blank\">Microsoft Documentation</a>"],
+    ["localTime", "Boolean", "Specify if the timestamp on the output timeseries is set to the timezone of the spatial area of interest."],
+    ["units", "String", "Units of the output timeseries. Valid options are: 'default', 'metric', 'imperial'"],
+    '''
+# Output return object are provided as a list of lists, each list containing 3 elements: column,
+# datatype and description.
+output_object = [
+    ["dataset", "String", "Primary dataset of the requested timeseries. Some API calls return more than one dataset, "
+                          "either for a workflow API or other relevent dataset."],
+    ["dataSource", "String", "Primary source of the requested timeseries."],
+    ["metaData", "Dictionary", "Metadata for the output timeseries, includes metadata from the source as well "
+                               "as HMS metadata."],
+    ["data", "Dictionary", "Output timeseries data is returned as a dictionary, where the key is the datetime stamp "
+                           "and value is a list of values for the source/dataset."]
+]
 
-    # HTTP API endpoint
-    http_API = [
-        ["POST", "/hms/rest/api/v3/meteorology/precipitation/"]
-    ]
+# HTTP API endpoint
+http_API = [
+    ["POST", "/hms/rest/api/v3/meteorology/precipitation/"]
+]
 
-    # Changelog is provided as a list of lists, where each list contains 3 elements: title, date, and a list of changes.
-    changelog = [
-        ["Version: 0.1 Beta", "May 29, 2019", ["Production UI and documentation initial setup."]]
-    ]
+# Changelog is provided as a list of lists, where each list contains 3 elements: title, date, and a list of changes.
+changelog = [
+    ["Version: 0.1 Beta", "May 29, 2019", ["Production UI and documentation initial setup."]]
+]
