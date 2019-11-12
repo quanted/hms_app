fc41473b0a044707ad761a31371b576b74bde004
Douglas Patton <50275427+DouglasPatton@users.noreply.github.com>
2019-11-12 08:06:41 -0500

    minor format changes to documentation

--- models/meteorology/precipitation_overview.py
+++ models/meteorology/precipitation_overview.py
@@ -76,17 +76,20 @@ class Precipitation:
         ["Source", "String", "Time-series data source (valid sources: nldas, gldas, daymet, ncei, prism, wgen, nwm)"],
         ["Start Date", "String", "Start date for the output timeseries. e.g., 01/01/2010"],
         ["End Date", "String", "End date for the output timeseries. e.g., 12/31/2010"],
-        ["Output Date Format", "String", "Format of the datetime stamp of the output timeseries. Valid options can be found "
-                                         "here: <a href=\"https://docs.microsoft.com/en-us/dotnet/api/system.datetime.tostring?view=netcore-2.2\" target=\"_blank\">Microsoft Documentation</a>"],
+        ["Output Date Format", "String", "Format of the datetime stamp of the output timeseries. Valid options can be"
+                                         " found here: <a href=\"https://docs.microsoft.com/en-us/dotnet/api/system."
+                                         "datetime.tostring?view=netcore-2.2\" target=\"_blank\">Microsoft "
+                                         "Documentation</a>"],
         ["Latitude", "Number", "Latitude coordinate for the output timeseries. e.g., 33.925575"],
         ["Longitude", "Number", "Longitude coordinate for the output timeseries. e.g., -83.356893"],
-        ["NCEI StationID", "String", "NOAA NCEI station identification number (available if source set to 'ncei'). e.g., GHCND:USW00013874 ."],
-
-        ["Temporal Resolution", "String", "Temporal resolution/timestep of the output timeseries. Options are limited by the "
-                                          "default timestep of the data source. All options are: 'default', 'daily', 'weekly', 'monthly'."],
+        ["NCEI StationID", "String", "NOAA NCEI station identification number (available if source set to 'ncei'). "
+                                     "e.g., GHCND:USW00013874 ."],
+        ["Temporal Resolution", "String", "Temporal resolution/timestep of the output timeseries. Options are limited"
+                                          " by the default timestep of the data source. All options are: 'default',"
+                                          " 'daily', 'weekly', 'monthly'."],
         ["Output Data Format", "String", "Format of the returned API object. Valid options are: 'json'."],
-        ["Local Time", "Boolean",
-         "Specify if the date/timestamp on the output timeseries is set to the local timezone of the spatial area of interest or to GMT."],
+        ["Local Time", "Boolean", "Specify if the date/timestamp on the output timeseries is set to the local timezone"
+                                  " of the spatial area of interest or to GMT."],
     ]
     '''--------------------discarded documentation:---------------------
     ["dateTimeSpan", "Dictionary", "Object holding the timeseries temporal input parameters "
